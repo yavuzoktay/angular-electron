305fc05e549c57eaa90979d647b34f730afbdd87
Yavuz Oktay <yavuz.oktay@ibtech.com.tr>
2020-05-25 00:02:52 +0300

    Revert "Pomdoro Angular Electron"
    This reverts commit 9219b4730b506fe6cc1f9b1252c3a152558a6788.

--- src/app/format-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatPipePipe } from './format-pipe.pipe';
-
-describe('FormatPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
